cd96c6d2e9ae580ba87435c5ade69ada7b472683
Kevin Weber <kevinweber@users.noreply.github.com>
2023-03-21 23:29:34 +0100

    Rearrange logo

--- client/index.js
+++ client/index.js
@@ -7,10 +7,10 @@ const html = htm.bind(h);
 function App() {
   return html`
     <div>
-      <div class="logo-wrapper">${logo()}</div>
+      <h1 class="wordmark">PunkStrat</h1>
       <p>We partner with founders who are shaping the future. Our services encompass principled <span class="highlight">strategies</span> and confidential executive <span class="highlight">coaching</span>.</p>
       <p>We prefer to keep a low profile ourselves and take on new clients <span class="highlight">by referral only</span>. Our sole focus is your success.</p>
-      <h1 class="wordmark">PunkStrat</h1>
+      <div class="logo-wrapper">${logo()}</div>
     </div>
   `;
 }
