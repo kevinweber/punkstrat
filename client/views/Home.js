05f39d576cac8a21eb333b2a71eda53e1a14062c
Kevin Weber <kevinweber@users.noreply.github.com>
2023-06-12 00:44:28 +0200

    Skip the referral sentence

--- client/views/Home.js
+++ client/views/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div class="home">
       <h1 class="title wordmark">PunkStrat</h1>
       <p>I co-innovate with <span class="highlight">purpose-driven leaders</span> to <span class="highlight">adapt and create</span> for the future. I do so by developing an <span class="highlight">innovation strategy</span> tailored to your business and by unlocking your team's potential through <span class="highlight">coaching</span>.</p>
-      <p>Read the <a href="/case-studies" title="PunkStrat Case Studies">case studies</a> to learn about my approach. To fully focus on ongoing client partnerships, new clients are added via referral only.</p>
+      <p>To learn about my approach, read the <a href="/case-studies" title="PunkStrat Case Studies">case studies</a>.</p>
       <div class="logo-wrapper">${logo()}</div>
     </div>
   `;
