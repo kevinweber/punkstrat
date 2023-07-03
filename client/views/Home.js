df9c436bed02fe0aef5d6d5fbf4915c81b46e377
Kevin Weber <kevinweber@users.noreply.github.com>
2023-07-03 22:59:46 +0200

    "Get to the point"

--- client/views/Home.js
+++ client/views/Home.js
@@ -8,8 +8,13 @@ export default function Home() {
   return html`
     <div class="home">
       <h1 class="title wordmark">PunkStrat</h1>
-      <p>I co-innovate with <span class="highlight">purpose-driven leaders</span> to <span class="highlight">adapt and create</span> for the future. I do so by developing an <span class="highlight">innovation strategy</span> tailored to your business and by unlocking your team's potential through <span class="highlight">coaching</span>.</p>
-      <p>To learn about my approach, read the <a href="/case-studies" title="PunkStrat Case Studies">case studies</a>.</p>
+      <p>I co-innovate with <span class="highlight">purpose-driven leaders</span> through <span class="highlight">business coaching</span>. Let me do us a favor: I skip my marketing gibberish. Your friends probably referred you here, ask them. Then let's get to it:</p>
+      <ol>
+        <li>You book a free 20-30 min coaching session with me.</li>
+        <li>We vibe – or we don't.</li>
+        <li>It's time to transform your business.</li>
+      </ol>
+      <p>To learn about my experience, read the <a href="/case-studies" title="PunkStrat Case Studies">case studies</a>.</p>
       <div class="logo-wrapper">${logo()}</div>
     </div>
   `;
