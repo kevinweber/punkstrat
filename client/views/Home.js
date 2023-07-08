a1b2b3673d6a139ae69a87818a4a1179888f94c8
Kevin Weber <kevinweber@users.noreply.github.com>
2023-07-08 05:18:26 +0200

    Further text refinement

--- client/views/Home.js
+++ client/views/Home.js
@@ -8,13 +8,9 @@ export default function Home() {
   return html`
     <div class="home">
       <h1 class="title wordmark">PunkStrat</h1>
-      <p>I co-innovate with <span class="highlight">purpose-driven leaders</span> through <span class="highlight">business coaching</span>. Let me do us a favor: I skip my marketing gibberish. Your friends probably referred you here, ask them. Then let's get to it:</p>
-      <ol>
-        <li>You book a free 20-30 min coaching session with me.</li>
-        <li>We vibe – or we don't.</li>
-        <li>It's time to transform your business.</li>
-      </ol>
-      <p>To learn about my experience, read the <a href="/case-studies" title="PunkStrat Case Studies">case studies</a>.</p>
+      <p>I partner with <span class="highlight">technical founders and executives</span> like CTOs, CIOs and CXOs to help them <span class="highlight">envision and deliver cutting-edge technology products and engineering culture</span> by providing coaching that unleashes their leadership skills, strategic thinking and innovation capabilities.</p>
+      <p>To learn more about my approach, read the <a href="/case-studies" title="PunkStrat Case Studies">case studies</a>.</p>
+      <p>If you are ready to take your technology leadership to the next level, schedule a discovery call with me today.</p>
       <div class="logo-wrapper">${logo()}</div>
     </div>
   `;
