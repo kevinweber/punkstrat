94ae36c51552cb689cc889b6286c86f69bd4784a
Kevin Weber <kevinweber@users.noreply.github.com>
2024-06-17 17:40:31 +0200

    Adjust wording from executive to leadership coaching

--- client/views/Home.js
+++ client/views/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
     <div class="home">
       <h1 class="title wordmark">PunkStrat</h1>
       <p><span class="highlight">Rethink Success, Rewrite the Rules.</span></p>
-      <p>PunkStrat℠ is executive coaching for tech leaders ready to reinvent themselves for long-term success and fulfillment.</p>
+      <p>PunkStrat℠ is coaching for tech leaders ready to reinvent themselves for long-term success and fulfillment.</p>
       <p>I combine 12+ years of tech experience with a personalized coaching approach to help you find your voice as a leader and drive meaningful impact while prioritizing well-being and personal growth.</p>     
       <!--<p>To learn more about our approach, read the <a href="/case-studies" title="PunkStrat Case Studies">case studies</a>.</p>-->
       <p>Let's rethink success, together. <a href="https://www.linkedin.com/in/kevinchrisweber/" title="Kevin Weber on LinkedIn">Contact me</a>.</p>
